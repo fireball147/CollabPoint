ca8efe49e99b6bcc8a832258735af28d21a2442e
fireball147 <niranjand147@gmail.com>
2022-03-03 01:00:27 +0530

    commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-window.addEventListener('resize', () => {
-    // We execute the same script as before
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-  });
+window.addEventListener("resize", () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+});
+
+function setFrame(e) {
+  if (e.target.id === "new_req") {
+    document
+      .getElementById("main-frame")
+      .setAttribute("src", "new_request.html");
+  }
+  if (e.target.id === "my_req") {
+    document
+      .getElementById("main-frame")
+      .setAttribute("src", "my_request.html");
+  }
+}
